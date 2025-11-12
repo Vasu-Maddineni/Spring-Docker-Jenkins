26afe1d96172e63eeeac7f709950a2bce571e014
Vasu-Maddineni <vasu.maddineni9226@gmail.com>
2025-11-12 23:00:58 +0530

    First commit

---
+++ src/test/java/com/Spring_Docker_Jenkins/SpringDockerJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_Docker_Jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
