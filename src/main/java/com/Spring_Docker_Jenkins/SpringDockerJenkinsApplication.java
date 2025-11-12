11f305f550db6241c34446f911d05395675ef29f
Vasu-Maddineni <vasu.maddineni9226@gmail.com>
2025-11-12 23:24:40 +0530

    first commit

--- src/main/java/com/Spring_Docker_Jenkins/SpringDockerJenkinsApplication.java
+++ src/main/java/com/Spring_Docker_Jenkins/SpringDockerJenkinsApplication.java
@@ -2,9 +2,17 @@ package com.Spring_Docker_Jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringDockerJenkinsApplication {
+	
+	@GetMapping
+	public String home() {
+		return "Welcome to mven automatic build !!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDockerJenkinsApplication.class, args);
